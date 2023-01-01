d05440f56ce3b15f18ce5904379c25cfd6c86b7b
Syndesi <soerenklein98@gmail.com>
2023-01-01 13:39:44 +0100

    initial commit

---
+++ src/Contract/DocumentInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Syndesi\ElasticDataStructures\Contract;
+
+interface DocumentInterface
+{
+    public function getCollection(): ?string;
+
+    public function setCollection(?string $collection): self;
+
+    public function getIdentifier(): mixed;
+
+    public function setIdentifier(mixed $identifier): self;
+
+    public function addProperty(string $name, mixed $value = null): self;
+
+    /**
+     * @param iterable<string, mixed> $properties
+     */
+    public function addProperties(iterable $properties): self;
+
+    public function hasProperty(string $name): bool;
+
+    public function getProperty(string $name): mixed;
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function getProperties(): array;
+
+    public function removeProperty(string $name): self;
+
+    public function removeProperties(): self;
+}
